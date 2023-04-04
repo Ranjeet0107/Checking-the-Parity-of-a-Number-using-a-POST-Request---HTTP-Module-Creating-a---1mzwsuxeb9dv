c9857f209fc5076aebd7b8f1ec884624ac4b946b
Ranjeet <ranjeet.ggi@gmail.com>
2023-04-04 20:10:45 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -12,6 +12,17 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
+     if(!value || Number.isNaN(value)){
+      res.statusCode=400;
+      res.end("Bad request");
+     }
+     if(value%2===0){
+      res.statusCode=200;
+      res.end(`The number ${value} is even`);
+     }else{
+      res.statusCode=404;
+      res.end(`The number ${value} is odd`);
+     }
 
    });
   }
